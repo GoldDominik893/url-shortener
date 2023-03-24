b1fb18e1cc53e5e42f1768f6cbba3449e86dd7ac
Dominik Wajda <admin@epicsite.xyz>
2023-03-24 20:25:00 +0000

    Update index.php

--- index.php
+++ index.php
@@ -6,14 +6,53 @@
   <body>
     <?php
     if($_POST['url']) {
-    ?>
-  
-    <?php } else { ?>
-    <form action="/" method="POST">
-      <label for="url">
+        $url = $_POST['url'];
+        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+        $random_string = '';
+        for ($i = 0; $i < 8; $i++) {
+            $index = rand(0, strlen($characters) - 1);
+            $random_string .= $characters[$index];
+        }
+
+        
+        $file = 'urls.txt';
+        $current = file_get_contents($file);
+        $current .= $url."\n".$random_string."\n";
+        file_put_contents($file, $current);
+
+        ?>
+        Your Shortened URL is: 
+    <?php 
+        $addr = $_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF']."?id=".$random_string;
+        echo '<a href="//'.$addr.'">'.$addr.'</a>';
+        } elseif($_GET['id']) {
+
+            $filename = 'urls.txt';
+            $search_string = $_GET['id'];
+            $handle = fopen($filename, 'r');
+            $line_number = 0;
+            while (($line = fgets($handle)) !== false) {
+                $line_number++;
+                if (strpos($line, $search_string) !== false) {
+                    break;
+                }
+            }
+            fclose($handle);
+            $handle = fopen($filename, 'r');
+            for ($i = 1; $i < $line_number-1; $i++) {
+                fgets($handle);
+            }
+            $line_contents = fgets($handle);
+            fclose($handle);
+            header("Location: $line_contents");
+            echo '<a href="'.$line_contents.'">'.$line_contents.'</a>';
+
+
+        } else { ?>
+    <form action="index.php" method="POST">
+      <label for="url">Enter URL</label>
       <input type="url" id="url" name="url">
     </form>
-    <?php }
-      ?>
+    <?php } ?>
   </body>
 </html>
